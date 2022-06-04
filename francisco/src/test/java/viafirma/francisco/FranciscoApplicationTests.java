d43c7d9dde75ff34bfdb81d3e78a65d9a63428ed
Francisco Fernández Angulo <fraferang@alum.us.es>
2022-06-05 01:53:03 +0200

    proyecto inicial

---
+++ francisco/src/test/java/viafirma/francisco/FranciscoApplicationTests.java
@@ -0,0 +1,13 @@
+package viafirma.francisco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranciscoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
